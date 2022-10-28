72ea7e1e5c47949dca60a042db0a67262384bdcc
Sayyora2021 <sayyorapardaeva@gmail.com>
2022-10-28 11:38:21 +0200

    cleaning

--- backend/middleware/password.js
+++ backend/middleware/password.js
@@ -13,9 +13,7 @@ passwordSchema
 .has().digits(2)                                //must have 2 difits (2 chiffres)
 .has().not().spaces()                           //No spaces
 .is().not().oneOf(['Passw0rd', 'Password123', 'Azerty1', 'Azerty2']); //Blacklist these values
-//userSchema.plugin(passwordValidator);
 
-//console.log("passwordSchema", passwordSchema)
 
 module.exports =(req, res, next)=> {
     if(passwordSchema.validate(req.body.password)){
