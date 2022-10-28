45adf2e7345cf48150c989b6e12d56c41dfbcd7d
Sayyora2021 <sayyorapardaeva@gmail.com>
2022-10-28 09:48:29 +0200

    last-correction

--- backend/app.js
+++ backend/app.js
@@ -12,13 +12,11 @@ const userRoutes = require('./routes/user');
 //installation de mongoDB
 const {DB_USER, DB_PASSWORD, DB_CLUSTER_NAME}= process.env;
 mongoose.connect(`mongodb+srv://${DB_USER}:${DB_PASSWORD}@${DB_CLUSTER_NAME}.mongodb.net/test?retryWrites=true&w=majority`,
-
-//mongoose.connect('mongodb+srv://ps:sapa@cluster0.2efuami.mongodb.net/test?retryWrites=true&w=majority',
   { useNewUrlParser: true,
     useUnifiedTopology: true })
   .then(() => console.log('Connexion à MongoDB réussie !'))
   .catch(() => console.log('Connexion à MongoDB échouée !'));
-console.log(process.env.DB_User);
+
 const app = express();
 
 app.use(express.json());
